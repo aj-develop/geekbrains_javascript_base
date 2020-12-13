100f08d996276f723f3f8b9223444be63b8a23dd
Alexander Jampolski <aj@Alexanders-MBP.fritz.box>
2020-12-13 12:51:02 +0100

    added hw8.1

--- 8/1 текст вместо картинки/app.js
+++ 8/1 текст вместо картинки/app.js
@@ -1,35 +1,41 @@
 'use strict';
 // 1. получите все кнопки и сохраните в переменную
+let buttons = document.querySelectorAll('button');
 
 // 1.1 затем проитерируйтесь по кнопкам и каждой из
 // них добавьте обработчик клика - функцию handleClick
-
+buttons.forEach(function (button) {
+    button.addEventListener('click', function (event) {
+        handleClick(event);
+    });
+});
 
 /**
  * Функция обрабатывает клик по кнопке в карточке товара и попеременно вызывает
  * функции для показа или скрытия текста о товаре.
- * @param {MouseEvent} clickedButtonEvent 
+ * @param {MouseEvent} clickedButtonEvent
  */
 function handleClick(clickedButtonEvent) {
     // 2. из объекта события получите ссылку на .product и
     // сохраните в переменную:
     // const cardNode = ;
+    const cardNode = clickedButtonEvent.target.parentNode;
 
     // 3. создайте литерал объекта со следующими свойствами:
     const card = {
-        wrap: , // здесь элемент с классом .product
-        img: , // здесь картинка внутри .product
-        productName: , // здесь .productName, который внутри .product
-        button: , // здесь button, который внутри .product
+        wrap: cardNode, // здесь элемент с классом .product
+        img: cardNode.querySelector('img'), // здесь картинка внутри .product
+        productName: cardNode.querySelector('div.productName'), // здесь .productName, который внутри .product
+        button: cardNode.querySelector('button'), // здесь button, который внутри .product
     };
 
     // 4. получаем текст на кнопке, которая внутри .product
-    
-    
-    if () { // 4.1 проверяем равняется ли этот текст строке "Подробнее"
+    let buttonText = clickedButtonEvent.target.innerHTML;
+
+    if (buttonText === "Подробнее") { // 4.1 проверяем равняется ли этот текст строке "Подробнее"
         // 4.2 если да, то передаем объект card в функцию showMoreText
         showMoreText(card);
-    } else if () { // 4.3 проверяем равняется ли текст на кнопке строке "Отмена"
+    } else if (buttonText === "Отмена") { // 4.3 проверяем равняется ли текст на кнопке строке "Отмена"
         // 4.4 если да, то передаем объект card в функцию hideMoreText
         hideMoreText(card);
     }
@@ -37,7 +43,7 @@ function handleClick(clickedButtonEvent) {
 
 /**
  * Функция скрывает текст с описанием товара.
- * @param {Object} card 
+ * @param {Object} card
  * @param {HTMLDivElement} card.wrap
  * @param {HTMLImageElement} card.img
  * @param {HTMLDivElement} card.productName
@@ -45,28 +51,29 @@ function handleClick(clickedButtonEvent) {
  */
 function hideMoreText(card) {
     // 5. картинке внутри .product ставим стиль display: block
-    
+    card.img.style.display = 'block';
     // 5.1 внутри .product находим элемент с классом .desc и удаляем его
-    
+    let divDesc = card.wrap.querySelector('div.desc');
+    card.wrap.removeChild(divDesc);
     // 5.2 кнопке, которая внутри .product ставим текст "Подробнее"
-    
+    card.button.innerText = 'Подробнее';
 }
 
 /**
  * Функция показывает текст с описанием товара.
- * @param {Object} card 
+ * @param {Object} card
  * @param {HTMLDivElement} card.wrap
  * @param {HTMLImageElement} card.img
  * @param {HTMLDivElement} card.productName
- * @param {HTMLButtonElement} card.button 
+ * @param {HTMLButtonElement} card.button
  */
 function showMoreText(card) {
     // 6. картинке внутри .product ставим display: none
-    
+    card.img.style.display = 'none';
     // 6.1 сохраняем произвольный текст в переменную
-    
+    let text = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.';
     // 6.2 внутри .product есть .productName, после него вставляем div.desc и текстом из переменной из п. 6.1
-    
+    card.productName.insertAdjacentHTML('afterend', `<div class="desc">${text}</div>`);
     // 6.3 внутри .product у кнопки текст ставим "Отмена"
-    
+    card.button.innerText = 'Отмена';
 }
